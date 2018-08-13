366adc5f6af5a266637c1bdb4455b90844a21afa
Jacob Berman <jacobberman1995@gmail.com>
2018-08-13 01:38:05 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,7 +33,7 @@ def bulk_tweet_shortener(tweet_array)
   
 end 
 
-def selective_tweet_shortner(tweet)
+def selective_tweet_shortener(tweet)
   if tweet.length > 140
     word_substituter(tweet)
   else 
